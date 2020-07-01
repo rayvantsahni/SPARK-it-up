0735a76334638cd0d473cad4a838b3e1b6141e82
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-07-02 02:01:31 +0530

    Create script.py

---
+++ RDDs from External Datasets/script.py
@@ -0,0 +1,10 @@
+# The variable file_path contains the path of the file on your local system.
+
+# Print the file_path
+print("The file_path is", file_path)
+
+# Create a fileRDD from file_path
+fileRDD = sc.textFile(file_path)
+
+# Check the type of fileRDD
+print("The file type of fileRDD is", type(fileRDD))
